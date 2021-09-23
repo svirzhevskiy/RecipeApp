44f72b0408c2042297198dd39ae1ab8d1c3b0e80
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-09-23 16:31:33 +0300

    Removed GetAll from BaseRepository

--- src/Core/Application/Repositories/IBaseRepository.cs
+++ src/Core/Application/Repositories/IBaseRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 
@@ -8,7 +7,6 @@ namespace Application.Repositories
     public interface IBaseRepository<T> where T : BaseEntity
     {
         Task<T> Create(T entity);
-        Task<IEnumerable<T>> GetAll(int take, int skip, Func<bool, T> filter);
         Task<T> Update(T entity);
         Task<T> GetById(Guid id);
         Task Delete(Guid id);
