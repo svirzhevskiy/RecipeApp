238e9995442eecc3ab7fbb2fdea7eabec3818837
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-09-23 17:25:40 +0300

    Added Database project. Added AppDbContext

---
+++ src/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
