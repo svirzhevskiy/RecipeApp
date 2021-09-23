b5f6c689f3cd1fc852f0a6ed999e1fdd4585ebb3
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-09-23 16:30:44 +0300

    Added Application project. Added BaseRepository

---
+++ src/Core/Application/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<T> Create(T entity);
+        Task<IEnumerable<T>> GetAll(int take, int skip, Func<bool, T> filter);
+        Task<T> Update(T entity);
+        Task<T> GetById(Guid id);
+        Task Delete(Guid id);
+    }
+}
