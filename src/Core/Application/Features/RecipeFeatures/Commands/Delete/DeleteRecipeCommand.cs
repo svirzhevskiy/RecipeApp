8fcc2add75d90e25a88ad8664567a4fefc968ed1
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-10-15 11:31:49 +0300

    Added delete recipe command

---
+++ src/Core/Application/Features/RecipeFeatures/Commands/Delete/DeleteRecipeCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Exceptions;
+using Application.Repositories;
+using Domain;
+using MediatR;
+
+namespace Application.Features.RecipeFeatures.Commands.Delete
+{
+    public class DeleteRecipeCommand : IRequest
+    {
+        public DeleteRecipeCommand(Guid id)
+        {
+            this.Id = id;
+        }
+        
+        public Guid Id { get; set; }
+        
+        public class Handler : IRequestHandler<DeleteRecipeCommand, Unit>
+        {
+            private readonly IBaseRepository<Recipe> _repository;
+
+            public Handler(IBaseRepository<Recipe> repository)
+            {
+                _repository = repository;
+            }
+
+            public async Task<Unit> Handle(
+                DeleteRecipeCommand request, 
+                CancellationToken cancellationToken)
+            {
+                var entity = await _repository.GetById(request.Id, cancellationToken);
+
+                if (entity is null)
+                {
+                    throw AppException.RecipeNotFound;
+                }
+
+                await _repository.Delete(entity, cancellationToken);
+                
+                return Unit.Value;
+            }
+        }
+    }
+}
--- src/Web/WebApi/Controllers/RecipeController.cs
+++ src/Web/WebApi/Controllers/RecipeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Features.RecipeFeatures.Commands.Create;
+using Application.Features.RecipeFeatures.Commands.Delete;
 using Application.Features.RecipeFeatures.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -31,5 +33,13 @@ namespace WebApi.Controllers
             CreateRecipeCommand command,
             CancellationToken cancellationToken)
             => Created("", await _mediator.Send(command, cancellationToken));
+
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(
+            [FromQuery]Guid id,
+            CancellationToken cancellationToken) 
+            => Ok(await _mediator.Send(new DeleteRecipeCommand(id), cancellationToken));
     }
 }
